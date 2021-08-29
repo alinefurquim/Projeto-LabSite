5233082d1421298b344f7982554a0489af21417b
Aline Furquim <aline1080@gmail.com>
2021-08-29 08:17:18 -0300

    LabSite project

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+
+    //validação campo obrigatório
+    const validar = () => {
+        
+        const nome = document.getElementById('nome');
+        const validacaoNome = document.getElementById('validacaoNome');
+
+        let validacaoOk = true;
+        
+        if(nome.value.length === 0) {
+            validacaoOk = false;
+            validacaoNome.innerHTML = 'Este campo é obrigatório';
+        }
+        validar();
+    };
+
+    //botão enviar
+//     const enviar = (idBotao) => {        
+//         const botao = document.getElementById(idBotao);
+//         botao.addEventListener('click', validar);
+//     };
+//    enviar('botao');
+};
