d1e919f15388fbdbc60e8c6ad0e9d852a8a604bb
Aline Furquim <aline1080@gmail.com>
2021-08-31 07:06:35 -0300

    js code for form

--- script.js
+++ script.js
@@ -1,23 +1,72 @@
-window.onload = () => {
 
-    //validação campo obrigatório
-    const validar = () => {
-        
-        const nome = document.getElementById('nome');
-        const validacaoNome = document.getElementById('validacaoNome');
+    let formValidator = {
+        handleSubmit:(event) => {
+            event.preventDefault();
+            let send = true;
 
-        let validacaoOk = true;
-        
-        if(nome.value.length === 0) {
-            validacaoOk = false;
-            validacaoNome.innerHTML = 'Este campo é obrigatório';
-        }       
+            let inputs = form.querySelectorAll('input');
+
+            formValidator.clearErrors();
+
+            for(let i = 0; i < inputs.length; i++ ) {
+                let input = inputs[i];
+                let check = formValidator.checkInput(input);
+                if(check !== true) {
+                    send = false;
+                    formValidator.showError(input, check)
+                }
+            }
+            
+            if(send) {
+                form.submit();
+            }
+        },
+        checkInput:(input) => {
+            let rules = input.getAttribute('data-rules');
+            if(rules !== null) {
+                rules = rules.split('|');
+                for(let k in rules) {
+                    let rulesDetails = rules[k].split('=');
+                    switch(rulesDetails[0]) {
+                        case 'required':
+                            if(input.value == '') {
+                                return "Campo obrigatório";
+                            }
+                        break;
+                        case 'min':
+                            if(input.value.length < rulesDetails[1]){
+                                return 'Campo deve ter pelo menos '+rulesDetails[1]+' caracteres';
+                            }
+                        break;
+                        case 'email':
+                            if(!input.value.includes('@')) {
+                                return 'Incluir o @ no email';
+                            }
+                    }
+                }
+            }
+            return true;
+        },
+        showError:(input, error) => {
+            input.style.borderColor = '#FF0000';
+            let errorElement = document.createElement('div');
+            errorElement.classList.add('error');
+            errorElement.innerHTML = error;
+
+            input.parentElement.insertBefore(errorElement, input.ElementSibling);
+        },
+        clearErrors:() => {
+            let inputs = form.querySelectorAll('input');
+                for(let i = 0; i < inputs.length; i++) {
+                    inputs[i].style = '';
+                }
+
+            let errorElements = document.querySelectorAll('.error');
+            for(let i = 0; i < errorElements.length; i++) {
+                errorElements[i].remove();
+            }
+        }
     };
-       
-    //botão enviar
-    const enviar = (idBotao) => {        
-        const botao = document.getElementById(idBotao);
-        botao.addEventListener('click', validar);
-    };
-   enviar('botao');
-};
+
+    let form = document.querySelector('.form');
+    form.addEventListener('submit', formValidator.handleSubmit);
